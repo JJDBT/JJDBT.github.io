0357c69ca96ea5c3564f685c18a638cb9eed84a2
JJDBT <2282447629@qq.com>
2026-09-15 21:39:52 +0800

    优化：侧边角色改圆形头像风格，Live2D换shizuku模型缩小位置，手机端隐藏作品页定位角色图

--- themes/jjdbt/source/js/main.js
+++ themes/jjdbt/source/js/main.js
@@ -143,14 +143,14 @@
     try {
       window.L2Dwidget.init({
         model: {
-          jsonPath: "https://cdn.jsdelivr.net/npm/live2d-widget-model-miku@1.0.5/assets/miku.model.json"
+          jsonPath: "https://cdn.jsdelivr.net/npm/live2d-widget-model-shizuku@1.0.5/assets/shizuku.model.json"
         },
         display: {
-          width: 170,
-          height: 330,
+          width: 140,
+          height: 280,
           position: "right",
-          hOffset: 0,
-          vOffset: 0
+          hOffset: -20,
+          vOffset: -20
         },
         mobile: {
           show: false
